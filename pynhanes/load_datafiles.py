0c12c4f25a72e380f4054d06fc74408c2b53f58d
poldrack <poldrack@gmail.com>
2020-07-05 15:07:37 -0700

    fixing variable naming?

--- pynhanes/load_datafiles.py
+++ pynhanes/load_datafiles.py
@@ -10,10 +10,9 @@ from time import sleep
 import numpy as np
 import string
 from bs4 import BeautifulSoup
-from utils import get_nhanes_year_code_dict, get_source_code_from_filepath
+from collections import Counter
 
-
-    
+from utils import get_nhanes_year_code_dict, get_source_code_from_filepath, EmptySectionError
 
 
 def download_raw_datafiles(datadir='../raw_data',
@@ -32,7 +31,7 @@ def download_raw_datafiles(datadir='../raw_data',
     if datasets is None:
         datasets = [
             'HSQ', 'DBQ', 'CBC', 'DLQ', 'HIQ', 'SLQ', 'DPQ', 'SMQRTU',
-            'PFQ', 'ACQ', 'BPX', 'DR2TOT', 'BMX', 'HDL', 'TCHOL',
+            'PFQ', 'ACQ', 'BPX', 'BMX', 'HDL', 'TCHOL',
             'PAQ', 'MCQ', 'FASTQX', 'DUQ', 'PBCD', 'DEMO', 'ECQ',
             'DXX', 'DR1TOT', 'DR2TOT', 'CDQ', 'DIQ', 'DEQ', 'GHB',
             'SMQ', 'WHQ', 'BPQ']
@@ -78,23 +77,29 @@ def load_raw_NHANES_data(datafile_path):
         datafiles = glob(str(datafile_path / '*XPT'))
         if len(datafiles) == 0:
             raise Exception('no data files available and unable to download')
+
     alldata = {}
-    data_dict = {}
+    metadata = {}
 
     for datafile in datafiles:
         with open(datafile, 'rb') as infile:
             print('opening', datafile)
+            source_code, metadata_df = get_metadata_from_xpt(datafile)
             # first need to load using xport to get metadata
-            xp = xport.v56.load(infile)
-            xp_key = list(xp.keys())[0]
-            metadata_df = xp[xp_key].contents
-            for index in metadata_df.index:
-                data_dict[metadata_df.loc[index, 'Variable']] = metadata_df.loc[index, 'Label']
+            metadata_df['Source'] = source_code
             # then load data itself with pandas
             df = pd.read_sas(datafile)
-            alldata[xp_key] = df.set_index('SEQN')
 
-    return(alldata, data_dict)
+            alldata[source_code] = df.set_index('SEQN')
+            metadata[source_code] = metadata_df 
+
+    return(alldata, metadata)
+
+
+def get_metadata_from_xpt(datafile):
+    xp = xport.v56.load(infile)
+    xp_key = list(xp.keys())[0]
+    return(xp_key, xp[xp_key].contents)
 
 
 def load_nhanes_documentation(doc_path):
@@ -105,7 +110,8 @@ def load_nhanes_documentation(doc_path):
     for docfile in docfiles:
         print('parsing docfile',docfile)
         doc_code = get_source_code_from_filepath(docfile)
-        variable_dfs[doc_code], variable_code_tables[doc_code] = parse_nhanes_html_docfile(docfile)
+        variable_dfs[doc_code], code_tables = parse_nhanes_html_docfile(docfile)
+        variable_code_tables.update(code_tables)
 
     variable_df = None
     for code in variable_dfs:
@@ -127,9 +133,12 @@ def join_all_dataframes(alldata):
 def parse_nhanes_html_docfile(docfile):
     variable_df = pd.DataFrame()
     variable_code_tables = {}
+    source_code = get_source_code_from_filepath(docfile)
 
     with open(docfile, 'r') as f:
         soup = BeautifulSoup('\n'.join(f.readlines()), 'html.parser')
+
+    # each variable is described in a separate div
     for section in soup.find_all('div'):
         try:
             variable_df, variable_code_tables = parse_html_variable_section(
@@ -137,23 +146,12 @@ def parse_nhanes_html_docfile(docfile):
         except EmptySectionError:
             pass
     
-    variable_df = variable_df.loc[variable_df.VariableName != 'SEQN',] 
+    variable_df = variable_df.loc[variable_df.index != 'SEQN_%s' % source_code, :] 
+    # check for duplicated long variable names
+    variable_df, variable_code_tables = deduplicate_long_variable_names_within_set(
+        variable_df, variable_code_tables)
+
     return((variable_df, variable_code_tables))
-
-
-def parse_html_variable_info_section(info):
-    infodict = {
-        i[0].text.strip(': ').replace(' ', ''): i[1].text
-        for i in zip(info.find_all('dt'), info.find_all('dd'))
-    }
-
-    infodict['VariableNameLong'] = ''.join([i.title() for i in infodict['SASLabel'].translate(str.maketrans('', '', string.punctuation)).split(' ')]) if 'SASLabel' in infodict else infodict['VariableName']
-
-    return(infodict)
-
-
-class EmptySectionError(Exception):
-    pass
 
 
 def parse_html_variable_section(section, variable_df, variable_code_tables, docfile):
@@ -168,21 +166,74 @@ def parse_html_variable_section(section, variable_df, variable_code_tables, docf
     infodict = parse_html_variable_info_section(info)
     assert title.get('id') == infodict['VariableName']
 
-    index_variable = 'VariableNameLong'
-    if infodict[index_variable] in variable_df.index:
-        infodict[index_variable] = '%s_%s' % (infodict[index_variable], source_code)
-
+    index_variable = 'VariableName'
+    infodict['index'] = '%s_%s' % (infodict[index_variable], source_code)        
+ 
     for key in infodict:
-        if key != index_variable:
+        if key != 'index':
+            # need to fix overlapping names for dietary recall
+            if source_code in ['DR1TOT', 'DR2TOT']:
+                infodict[key] = '%s_%s' % (infodict[key], source_code.replace('TOT', ''))
             variable_df.loc[infodict[index_variable], key] = infodict[key]
 
     table = section.find('table')
     if table is not None:
-        variable_code_tables[infodict[index_variable]] = pd.read_html(str(table))[0]
+        variable_code_tables[infodict['index']] = pd.read_html(str(table))[0]
 
     variable_df['Source'] = source_code
     return((variable_df, variable_code_tables))
 
+
+# for deduplicating within a single variable set
+def deduplicate_long_variable_names_within_set(variable_df):
+    variable_df = variable_df.query('VariableNameLong != "RespondentSequenceNumber"')
+    variable_counts = variable_df.VariableNameLong.value_counts()
+    repeated_variables = variable_counts[variable_counts > 1]
+    repeated_df = variable_df[variable_df.VariableNameLong.isin(repeated_variables.index)]  
+    for ctr, index in enumerate(repeated_df.index):
+        variable_df.loc[index, 'VariableNameLong'] = '%s_%d' % (
+            variable_df.loc[index, 'VariableNameLong'], ctr + 1)
+
+    return(variable_df)
+
+
+# for deduplicating once everything is combined
+def deduplicate_long_variable_names_across_sets(variable_df):
+    variable_df = variable_df.query('VariableNameLong != "RespondentSequenceNumber"')
+    variable_counts = variable_df.VariableNameLong.value_counts()
+    repeated_variables = variable_counts[variable_counts > 1]
+    repeated_df = variable_df[variable_df.VariableNameLong.isin(repeated_variables.index)]  
+    for ctr, index in enumerate(repeated_df.index):
+        variable_df.loc[index, 'VariableNameLong'] = '%s_%s' % (
+            variable_df.loc[index, 'VariableNameLong'],
+            variable_df.loc[index, 'Source'])
+
+    return(variable_df)
+
+def parse_html_variable_info_section(info):
+    infodict = {
+        i[0].text.strip(': ').replace(' ', ''): i[1].text
+        for i in zip(info.find_all('dt'), info.find_all('dd'))
+    }
+
+    infodict['VariableNameLong'] = ''.join([i.title() for i in infodict['SASLabel'].translate(str.maketrans('', '', string.punctuation)).split(' ')]) if 'SASLabel' in infodict else infodict['VariableName']
+
+    return(infodict)
+
+
+def rename_nhanes_vars(nhanes_df, variable_df):
+    rename_dict = {}
+    for i in variable_df.index:
+        rename_dict[i] = variable_df.loc[i, 'VariableNameLong']
+    return(nhanes_df.rename(columns=rename_dict))
+
+
+def recode_nhanes_vars(nhanes_df, variable_df, variable_code_tables):
+    for variable in nhanes_df.columns:
+        variable_info = variable_df.loc[variable,]
+        if variable not in variable_code_tables:
+            print('missing', variable)
+    
 
 def save_combined_data(nhanes_df, output_path='../combined_data'):
     combined_data_path = Path(output_path)
@@ -192,21 +243,20 @@ def save_combined_data(nhanes_df, output_path='../combined_data'):
     nhanes_df.to_csv(combined_data_path / str('NHANES_data_%s.tsv' % year), sep='\t')
 
 
-def rename_nhanes_vars(nhanes_df, variable_df):
-    rename_dict = {}
-    for i in variable_df.index:
-        rename_dict[variable_df.loc[i, 'VariableName']] = i
-
 if __name__ == "__main__":
     year = '2017-2018'
     datafile_path = Path('../raw_data/%s' % year)
-    alldata, data_dict = load_raw_NHANES_data(datafile_path)
+    alldata, metadata = load_raw_NHANES_data(datafile_path)
 
     doc_path = Path('../data_docs/%s' % year)
     variable_df, variable_code_tables = load_nhanes_documentation(doc_path)
 
+    variable_df = deduplicate_long_variable_names_across_sets(variable_df)
+
     nhanes_df = join_all_dataframes(alldata)
 
-    nhanes_df_renamed = rename_nhanes_vars(nhanes_df, variable_df)
+    nhanes_df_recoded = recode_nhanes_vars(nhanes_df, variable_code_tables)
 
-    save_combined_data(nhanes_df)
+    nhanes_df_renamed = rename_nhanes_vars(nhanes_df_recoded, variable_df)
+
+    save_combined_data(nhanes_df_renamed)
