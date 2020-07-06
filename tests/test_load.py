ec8dc51e936cd43d91fbe1ba7c76b19cd4b44c0d
poldrack <poldrack@gmail.com>
2020-07-06 14:23:14 -0700

    fix metadata test

--- tests/test_load.py
+++ tests/test_load.py
@@ -4,3 +4,8 @@ def test_load_data():
     df = load_NHANES_data(year='2017-2018')
     assert df.shape[0] == 8366
     assert df.shape[1] == 197
+
+def test_load_metadata():
+    df = load_NHANES_metadata(year='2017-2018')
+    assert df.shape[0] == 865
+    assert df.shape[1] == 16
