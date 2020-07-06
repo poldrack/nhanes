d713d18ecafb604be2df510ea4afce32e05463fa
poldrack <poldrack@gmail.com>
2020-07-06 14:57:38 -0700

    add test_match

--- tests/test_load.py
+++ tests/test_load.py
@@ -8,4 +8,10 @@ def test_load_data():
 def test_load_metadata():
     df = load_NHANES_metadata(year='2017-2018')
     assert df.shape[0] == 865
-    assert df.shape[1] == 16
+    assert df.shape[1] == 15
+
+def test_match():
+    data_df = load_NHANES_data(year='2017-2018')
+    metadata_df = load_NHANES_metadata(year='2017-2018')
+    # make sure column names in data match index in metadata exactly
+    not set(data_df.columns).difference(metadata_df.index)
