1a3ec208722af67dcfc3ed9a56d6d41df24c8448
poldrack <poldrack@gmail.com>
2020-07-06 15:42:27 -0700

    add open_dataset_page

--- nhanes/load.py
+++ nhanes/load.py
@@ -4,6 +4,8 @@ functions to load combined data
 
 import pkg_resources
 import pandas as pd
+import webbrowser
+from .utils import get_nhanes_year_code_dict
 
 
 def load_NHANES_data(year='2017-2018'):
@@ -12,7 +14,7 @@ def load_NHANES_data(year='2017-2018'):
 
     Parameters:
     -----------
-    year: string, denotes year code for data 
+    year: string, denotes year code for data
           (default = '2017-2018')
 
     Returns:
@@ -31,7 +33,7 @@ def load_NHANES_metadata(year='2017-2018'):
 
     Parameters:
     -----------
-    year: string, denotes year code for data 
+    year: string, denotes year code for data
           (default = '2017-2018')
 
     Returns:
@@ -41,3 +43,19 @@ def load_NHANES_metadata(year='2017-2018'):
     datafile = pkg_resources.resource_filename(
         'nhanes', 'combined_data/%s/NHANES_metadata_%s.tsv' % (year, year))
     return(pd.read_csv(datafile, sep='\t', index_col=0))
+
+
+def open_dataset_page(dataset, year='2017-2018'):
+    """
+    open the web page describing a particular dataset
+    - the dataset is listed in the Source variable with the metadata
+
+    Parameters:
+    -----------
+    dataset: string, the code for the individual dataset
+    year: the year code for the dataset
+
+    """
+    year_code = get_nhanes_year_code_dict()[year]
+    url = 'https://wwwn.cdc.gov/Nchs/Nhanes/%s/%s_%s.htm' % (year, dataset, year_code)
+    webbrowser.open(url)
